dd36478ad3b981e30227551f5cd09c0ffc5ae10b
mireureu <j96410@naver.com>
2023-09-21 16:24:59 +0900

    20230921

--- src/router.js
+++ src/router.js
@@ -2,21 +2,28 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Logout from "./pages/Logout";
+import Auctionpost from "./pages/Auctionpost1";
+import Auctionpost1 from "./pages/Auctionpost1";
+import App from "./pages/App";
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout/>,
-    //     children: [
-    //         {
-    //             index: true,
-    //             element: <App/>
-    //         },
-            
-    //         {
-    //         path: "watch", element: <Watch/>
-    //         }, 
+        children: [
+            {
+                index: true,
+        
+            },{
+              path:"/auction",
+              element:<Auctionpost1 />
 
-    // ]
+            },{
+              path:"/detail",
+              element:<App/>
+            }
+          
+
+    ]
     },
     {
         path:"/logout",
