bd3baae01c1616b4515bf54c4fd620e5d646fa80
dclown13 <w6092w@gmail.com>
2023-09-25 17:49:59 +0900

    09-25

--- src/Home.js
+++ src/Home.js
@@ -1,66 +1,104 @@
 // import Carousel from 'react-bootstrap/Carousel';
 // import images from '../src/components/1.png';
 import styled from "styled-components";
+// import { getAuctionBoard } from "./api/auctionBoard";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faWrench } from "@fortawesome/free-solid-svg-icons";
 
+
 const Main = styled.div`
   display: grid;
-  grid-template-columns: 1fr 5fr 1fr;
-  grid-template-rows: auto 150px;
-  grid-template-areas: 
-    "g-left g-gonggo g-right"
+  margin: 30px;
+  grid-template-columns: 1fr 4fr 1fr;
+  grid-template-rows: 80px 550px 150px;
+  grid-template-areas:
+    "g-left g-banner g-right" 
+    "g-left g-newitem g-right"
     "g-lower g-lower g-lower";
+  gap: 20px;
 `;
 
-const Left = styled.div`
-  grid-area: g-left;
-`;
-
-const GongGo = styled.div`
-  grid-area: g-gonggo;
-  width: 100%;
-  height: 50vh;
-  
-
-`;
-
-const Right = styled.div`
-  grid-area: g-right;
-`;
-
-const Lower = styled.div`
-  grid-area: g-lower;
-`;
-
-
-const Hot = styled.div`
+const Banner = styled.div`
+  grid-area: g-banner;
   border: 1px solid black;
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: rgba(255, 217, 250);
+  width: 80%;
+  margin: 0 auto;
+`;
 
-  .hot-box {
-    width: 150px;
-    height: 200px;
-    background-color: rgba(172, 94, 192);
+const Left = styled.div`
+  grid-area: g-left;
+  border: 1px solid black;
+`;
+
+const NewItem = styled.div`
+  grid-area: g-newitem;
+  width: 100%;
+  height: 50vh;
+`;
+
+const Right = styled.div`
+  grid-area: g-right;
+  border: 1px solid black;
+`;
+
+const Lower = styled.div`
+  grid-area: g-lower;
+  border: 1px solid black;
+`;
+
+
+const News = styled.div`
+  /* border: 1px solid black; */
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  justify-content: space-between;
+  gap: 10px;
+  margin: 0 auto;
+  max-width: 1000px;
+
+  .new-box {
+    width: 200px;
+    height: 250px;
+    background-color: rgba(234, 234, 234);
     border: 1px solid black;
+    border-radius: 5%;
     transition: 0.5s;
     position: relative;
     z-index: 1;
-    background-image: null;
+    /* background-image: none; */
 
-    .hot-font {
+    .new-font {
+      
       position: absolute;
-      width: 100%;
-      height: 30%;
+      width: 80%;
+      left: 10%;
+      height: auto;
       text-align: center;
       bottom: 0;
       line-height: 1;
+      
+
+      h5 {
+
+      }
+
+      p {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 5px;
+        height: 30px;
+        border: 1px solid black;
+        border-radius: 10px;
+      }
     }
   }
 
-  .hot-box:hover {
+  .new-box:hover {
     transform: scale(1.5);
     
     transform-origin: center;
@@ -77,9 +115,9 @@ const Hot = styled.div`
 
 const Home=()=> {
 
-  // const Hot = () => {
-    
-  // };
+  // const auctionBoardAPI = async () => {
+  //   const result = await getAuctionBoard();
+  // }
 
   return (
     <Main className='div-container'>
@@ -88,46 +126,103 @@ const Home=()=> {
 
       </Left>
       
-      <GongGo className='div-item'>
+      <Banner>
+        <h1>[ 주간 핫! 베스트 상품 ]</h1>
+        {/* <h1>[ 주간 New 경매! ]</h1> */}
+      </Banner>
 
-        <Hot className='hot-container'>
-          <div className='hot-box'>
-            <div className='hot-font'>
-              <p>게시글 제목</p>
-              <p>마감시간</p>
+      <NewItem className='div-item'>
+
+        <News className='new-container'>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
             </div>
           </div>
-          <div className='hot-box'>
-            <div className='hot-font'>
-              <p>게시글 제목</p>
-              <p>마감시간</p>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
             </div>
           </div>
-          <div className='hot-box'>
-            <div className='hot-font'>
-              <p>게시글 제목</p>
-              <p>마감시간</p>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
             </div>
           </div>
-          <div className='hot-box'>
-            <div className='hot-font'>
-              <p>게시글 제목</p>
-              <p>마감시간</p>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
             </div>
           </div>
-          <div className='hot-box'>
-            <div className='hot-font'>
-              <p>게시글 제목</p>
-              <p>마감시간</p>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
             </div>
           </div>
-          <div className='hot-box'>
-            <div className='hot-font'>
-              <p>게시글 제목</p>
-              <p>마감시간</p>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
             </div>
           </div>
-        </Hot>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 :<span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
+            </div>
+          </div>
+          <div className='new-box'>
+            <div className='new-font'>
+              <h5>게시글 제목</h5>
+              <p>
+                마감시간 : <span>30:05:20</span>
+              </p>
+              <p>
+                현재가 : <span>30,000</span>원
+              </p>
+            </div>
+          </div>
+        </News>
 
         {/* <Carousel>
           <Carousel.Item interval={5000}>
@@ -168,7 +263,7 @@ const Home=()=> {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel> */}
-      </GongGo>
+      </NewItem>
 
       <Right className='div-item'>
 
