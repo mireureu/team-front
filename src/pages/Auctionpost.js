48799b2b829fc4dafa3b11e66e73cac5ab8f48ac
wonku19 <133620204+wonku19@users.noreply.github.com>
2023-10-19 17:36:44 +0900

    1019

--- src/pages/Auctionpost.js
+++ src/pages/Auctionpost.js
@@ -5,6 +5,7 @@ import { getPost } from "../api/addpost";
 const Auctionpost = () => {
   const [auctionPost, setAuctionPost] = useState(null);
   const { auctionNo } = useParams();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
     const fetchAuctionPost = async () => {
@@ -21,6 +22,21 @@ const Auctionpost = () => {
     }
   }, [auctionNo]);
 
+  const nextImage = () => {
+    if (
+      auctionPost?.imageList &&
+      currentImageIndex < auctionPost.imageList.length - 1
+    ) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  };
+
+  const previousImage = () => {
+    if (auctionPost?.imageList && currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
+
   return (
     <div>
       <h1>Auction Post</h1>
@@ -29,6 +45,19 @@ const Auctionpost = () => {
           <h2>{auctionPost.auctionTitle}</h2>
           <p>상품 설명: {auctionPost.itemDesc}</p>
           <p>현재 가격: {auctionPost.currentPrice}원</p>
+          <div>
+            {auctionPost.imageList && auctionPost.imageList.length > 0 && (
+              <div>
+                <img
+                  src={auctionPost.imageList[currentImageIndex]}
+                  alt={`Image ${currentImageIndex + 1}`}
+                  style={{ maxWidth: "100%" }}
+                />
+                <button onClick={previousImage}>이전 이미지</button>
+                <button onClick={nextImage}>다음 이미지</button>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
