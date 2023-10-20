1fa692d5697777a5bc88bd9f610bfff769977ad8
dclown13 <w6092w@gmail.com>
2023-10-20 12:13:30 +0900

    10-20

--- src/api/auctionBoard.js
+++ src/api/auctionBoard.js
@@ -1,25 +1,36 @@
 import axios from "axios";
+import { async } from "q";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+    baseURL: "http://localhost:8080/api/",
 });
 
-export const getAuctionBoard = async () => {
-  return null;
+
+export const getAuctionBoard = async () => { 
+    return null;
 };
 
 export const getHotList = async () => {
-  return await instance.get("public/auction/hot");
+    return await instance.get("public/auction/hot");
 };
+
+export const getNewList = async () => {
+    return await instance.get("public/auction/new");
+};
+
 
 export const getCategories = async () => {
-  return await instance.get("public/category");
-};
+    return await instance.get("public/category");
+}
 
-export const getItem = async (page, category) => {
-  let url = `public/auction?page=${page}`;
-  if (category !== null) {
-    url += `&category=${category}`;
-  }
-  return await instance.get(url);
-};
+ 
+
+ export const getItem = async (page, category) => {
+    let url = `public/auction?page=${page}`;
+    if (category !== null) {
+      url += `&category=${category}`;
+    }
+    return await instance.get(url);
+  };
+  
+
